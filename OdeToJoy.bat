b8f547a330e282a9e1ae2392e204a916255c9279
Peter Miller <agentorange96@gmail.com>
2018-12-07 17:17:48 -0500

    Example using Bepis to play music
    Play Ode to Joy when run

---
+++ OdeToJoy.bat
@@ -0,0 +1,87 @@
+Rem	This file demonstrates Bepis' ability to play music
+Rem	These frequency values were tuned on an Asrock Z87 Killer motherboard
+Rem	With a 1984 IBM PC speaker
+Rem	Frequency values may vary by configuration
+Rem	You may use "Bepis T" and an instrument tuner to find your own values
+Rem	Ode to Joy - Ludwig Van Beethoven - D Major
+
+Rem		Frequency	Time
+Bepis.exe	3400		500	F#
+Bepis.exe	3400		500	F#
+Bepis.exe	3059		500	G
+Bepis.exe	2895		500	A
+Bepis.exe	2895		500	A
+Bepis.exe	3059		500	G
+Bepis.exe	3400		500	F#
+Bepis.exe	3606		500	E
+Bepis.exe	3829		500	D
+Bepis.exe	3829		500	D
+Bepis.exe	3606		500	E
+Bepis.exe	3400		500	F#
+Bepis.exe	3400		500	F#
+Bepis.exe	1		250	Rest
+Bepis.exe	3606		250	E
+Bepis.exe	3606		500	E
+Bepis.exe	1		500	Rest
+Bepis.exe	3400		500	F#
+Bepis.exe	3400		500	F#
+Bepis.exe	3059		500	G
+Bepis.exe	2895		500	A
+Bepis.exe	2895		500	A
+Bepis.exe	3059		500	G
+Bepis.exe	3400		500	F#
+Bepis.exe	3606		500	E
+Bepis.exe	3829		500	D
+Bepis.exe	3829		500	D
+Bepis.exe	3606		500	E
+Bepis.exe	3400		500	F#
+Bepis.exe	3606		500	E
+Bepis.exe	1		250	Rest
+Bepis.exe	3829		250	D
+Bepis.exe	3829		500	D
+Bepis.exe	1		500	Rest
+Bepis.exe	3606		500	E
+Bepis.exe	3606		500	E
+Bepis.exe	3400		500	F#
+Bepis.exe	3829		500	D
+Bepis.exe	3606		500	E
+Bepis.exe	3400		250	F#
+Bepis.exe	3059		250	G
+Bepis.exe	3400		500	F#
+Bepis.exe	3829		500	D
+Bepis.exe	3606		500	E
+Bepis.exe	3400		250	F#
+Bepis.exe	3059		250	G
+Bepis.exe	3400		500	F#
+Bepis.exe	3606		500	E
+Bepis.exe	3829		500	D
+Bepis.exe	3606		500	E
+Bepis.exe	2895		500	A
+Bepis.exe	1		500	Rest
+Bepis.exe	3400		500	F#
+Bepis.exe	3400		500	F#
+Bepis.exe	3059		500	G
+Bepis.exe	2895		500	A
+Bepis.exe	2895		500	A
+Bepis.exe	3059		500	G
+Bepis.exe	3400		500	F#
+Bepis.exe	3606		500	E
+Bepis.exe	3829		500	D
+Bepis.exe	3829		500	D
+Bepis.exe	3606		500	E
+Bepis.exe	3400		500	F#
+Bepis.exe	3606		500	E
+Bepis.exe	1		250	Rest
+Bepis.exe	3829		250	D
+Bepis.exe	3829		500	D
+
+
+rem		Note		Freq
+rem		D		3829
+rem		E		3606
+rem		F#		3400
+rem		G		3059
+rem		A		2895
+rem		B		2487
+rem		C#		2360
+rem		D		2041
